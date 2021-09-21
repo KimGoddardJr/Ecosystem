3cfd539c3cc30fb616abece5c342dbce38533a65
IAS-MDMA\IAAdmin <ia.space@zhdk.ch>
2021-09-21 16:22:08 +0200

    ripped out CMAKE

--- ecosystem/main.py
+++ ecosystem/main.py
@@ -34,9 +34,16 @@ import os
 import platform
 import subprocess
 import sys
-from .environment import Tool, Environment
-from .preset import Preset
-from .settings import MAKE_COMMAND, MAKE_TARGET
+
+try:
+    from ecosystem.environment import Tool, Environment
+    from ecosystem.preset import Preset
+except ImportError as e:
+    print("Import Error: ",e)
+    print("set the PYTHONPATH to the directory where your ecosystem is located")
+    
+#from .settings import MAKE_COMMAND, MAKE_TARGET
+
 import json
 
 _ON_WINDOWS = (platform.system().lower() == 'windows')
@@ -94,7 +101,7 @@ def call_process(arguments):
         subprocess.call(arguments, shell=True)
     else:
         subprocess.call(arguments)
-
+"""
 
 def build(tools=None, force_rebuild=False, quick_build=False, deploy=False):
     tools = tools or []
@@ -117,16 +124,16 @@ def build(tools=None, force_rebuild=False, quick_build=False, deploy=False):
             MAKE_COMMAND.append("package")
 
         call_process(MAKE_COMMAND)
-
+"""
 
 def run(tools=None, run_application=None, extra_args=None):
     tools = tools or []
-    extra_args = extra_args or []
+    # extra_args = extra_args or []
     env = Environment(tools)
-    cmd = [run_application] + extra_args
+    # cmd = [run_application] + extra_args
     if env.success:
         env.set_env(os.environ)
-        call_process(cmd)
+        call_process([run_application])
 
 
 def set_environment(tools=None):
@@ -155,6 +162,8 @@ Example:
                         help='specify a list of tools required separated by commas')
     parser.add_argument('-l', '--listtools', action='store_true',
                         help='list the available tools')
+    
+    """
     parser.add_argument('-b', '--build', action='store_true',
                         help='run the desired build process')
     parser.add_argument('-d', '--deploy', action='store_true',
@@ -163,6 +172,8 @@ Example:
                         help='force the full CMake cache to be rebuilt')
     parser.add_argument('-m', '--make', action='store_true',
                         help='just run make')
+    """                    
+    
     parser.add_argument('-r', '--run', type=str, default=None,
                         help='run an application')
     parser.add_argument('-s', '--setenv', action='store_true',
@@ -187,11 +198,15 @@ Example:
         if args.listtools:
             import pprint
             pprint.pprint(list_available_tools(), width=1)
+            
+            """
         elif args.build:
             if args.deploy:
                 build(tools, True, False, args.deploy)
             else:
                 build(tools, args.force, args.make, args.deploy)
+            """
+        
         elif args.run is not None:
             run(tools, args.run, extra)
         elif args.setenv:
